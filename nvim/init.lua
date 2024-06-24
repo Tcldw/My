c786f590fbfad9d9e3ed0500a136131dde0de200
Tcldw <2998965926@qq.com>
2024-06-24 23:50:55 +0800

    nvim的更改

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("config.options")
+require("config.lazzy")
+require("config.autocmds")
+vim.cmd.colorscheme("base16-tender")
---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,6 @@
+local set = vim.o
+set.number = true 			-- 行号
+set.relativenumber = true		-- 相对行号
+set.clipboard = "unnamed"    		-- 剪切板
+set.termguicolors = true        	-- 终端真颜色
+
